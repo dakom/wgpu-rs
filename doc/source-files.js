774223105bbea0d42a36234d88f85c79a9b18c28
dakom <dakom@users.noreply.github.com>
2020-06-18 15:55:44 +0000

    Deploying to gh-pages from  @ 96a4aad91e777a87fef3f24284e7b51b4c0098d8 🚀

---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["wgpu"] = {"name":"","dirs":[{"name":"backend","files":["direct.rs","mod.rs","native_gpu_future.rs"]},{"name":"util","files":["mod.rs"]}],"files":["lib.rs","macros.rs"]};
+createSourceSidebar();
